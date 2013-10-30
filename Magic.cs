f0da6915ae5d647f8dc6f24df9ed6983db92f38f
Josef Eklann <josef.eklann@gmail.com>
2013-10-30 08:21:08 +0100

    Start of the git-repo

---
+++ Magic.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+// fileidentifier.native.dll is build from this project: https://github.com/glensc/file
+
+// Releases of UNIX file can be found here: ftp://ftp.astron.com/pub/file/
+
+namespace NetClean.MimeReader
+{
+    public class Magic : IDisposable
+    {
+        public class MagicException : System.Exception
+        {
+            override public string Message { 
+                get{
+                    return string.Format(CultureInfo.InvariantCulture, "{0}: {1}", errno, error);
+                } 
+            }
+
+            public MagicException(int errno, string error)
+            {
+                this.errno = errno;
+                this.error = error;
+            }
+            public int errno;
+            public string error;
+        }
+
+        public const int MAGIC_NONE = 0x000000; /* No flags */
+        public const int MAGIC_DEBUG = 0x000001; /* Turn on debugging */
+        public const int MAGIC_SYMLINK = 0x000002; /* Follow symlinks */
+        public const int MAGIC_COMPRESS = 0x000004; /* Check inside compressed files */
+        public const int MAGIC_DEVICES = 0x000008; /* Look at the contents of devices */
+        public const int MAGIC_MIME_TYPE = 0x000010; /* Return the MIME type */
+        public const int MAGIC_CONTINUE = 0x000020; /* Return all matches */
+        public const int MAGIC_CHECK = 0x000040; /* Print warnings to stderr */
+        public const int MAGIC_PRESERVE_ATIME = 0x000080; /* Restore access time on exit */
+        public const int MAGIC_RAW = 0x000100; /* Don't translate unprintable chars */
+        public const int MAGIC_ERROR = 0x000200; /* Handle ENOENT etc as real errors */
+        public const int MAGIC_MIME_ENCODING = 0x000400; /* Return the MIME encoding */
+        public const int MAGIC_MIME = (MAGIC_MIME_TYPE | MAGIC_MIME_ENCODING);
+        public const int MAGIC_APPLE = 0x000800; /* Return the Apple creator and type */
+        public const int MAGIC_NO_CHECK_COMPRESS = 0x001000; /* Don't check for compressed files */
+        public const int MAGIC_NO_CHECK_TAR = 0x002000; /* Don't check for tar files */
+        public const int MAGIC_NO_CHECK_SOFT = 0x004000; /* Don't check magic entries */
+        public const int MAGIC_NO_CHECK_APPTYPE = 0x008000; /* Don't check application type */
+        public const int MAGIC_NO_CHECK_ELF = 0x010000; /* Don't check for elf details */
+        public const int MAGIC_NO_CHECK_TEXT = 0x020000; /* Don't check for text files */
+        public const int MAGIC_NO_CHECK_CDF = 0x040000; /* Don't check for cdf files */
+        public const int MAGIC_NO_CHECK_TOKENS = 0x100000; /* Don't check tokens */
+        public const int MAGIC_NO_CHECK_ENCODING = 0x200000; /* Don't check text encodings */
+
+        [DllImport("fileidentifier.native.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern unsafe IntPtr magic_open(int flags);
+
+        [DllImport("fileidentifier.native.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern unsafe int magic_load(IntPtr m, string file);
+
+        [DllImport("fileidentifier.native.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe private extern static IntPtr magic_file(IntPtr m, string filename);
+
+        [DllImport("fileidentifier.native.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe private extern static void magic_close(IntPtr m);
+
+        [DllImport("fileidentifier.native.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe private extern static IntPtr magic_error(IntPtr m);
+
+        [DllImport("fileidentifier.native.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe private extern static int magic_errno(IntPtr m);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet=CharSet.Unicode)]
+        public static extern int GetShortPathNameW(string lpszLongPath, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpszShortPath, [MarshalAs(UnmanagedType.U4)] int cchBuffer);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern int GetFullPathNameW(string lpFileName, [MarshalAs(UnmanagedType.U4)] int nBufferLength, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpBuffer, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpFilePart);
+
+        unsafe IntPtr m;
+        Object magicLock = new Object();
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        unsafe protected virtual void Dispose(bool disposeManaged)
+        {
+            lock (magicLock)
+            {
+                if (m != IntPtr.Zero)
+                {
+                    magic_close(m);
+                    m = IntPtr.Zero;
+                }
+            }
+
+            if (disposeManaged) { }
+        }
+
+        ~Magic() {
+            Dispose(false);
+        }
+
+        unsafe public string GetInfo(string file)
+        {
+            lock (magicLock)
+            {
+                if (m != IntPtr.Zero)
+                {
+                    IntPtr desc = magic_file(m, file);
+                    if (desc != IntPtr.Zero)
+                    {
+                        return Marshal.PtrToStringAnsi(desc);
+                    }
+                    throw new MagicException(magic_errno(m), Marshal.PtrToStringAnsi(magic_error(m)));
+                }
+
+                throw new MagicException(-1, "Invalid magic handle");
+            }
+        }
+
+        unsafe public Magic(string database, int flags)
+        {
+            lock (magicLock)
+            {
+                m = magic_open(flags | MAGIC_ERROR);
+
+                if (m == IntPtr.Zero)
+                {
+                    throw new MagicException(-1, "Couldn't create magic handle");
+                }
+
+                int ret = magic_load(m, database);
+
+                if (ret < 0)
+                {
+                    throw new MagicException(magic_errno(m), Marshal.PtrToStringAnsi(magic_error(m)));
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace NetClean.MimeReader
+{
+	class Program
+	{
+		private static Magic fileMagic;
+		private static Magic mimeMagic;
+
+		private static StreamReader input;
+        private static StreamWriter output;
+        private static StreamWriter error;
+
+		private static JavaScriptSerializer serializer;
+
+        [Flags]
+        public enum ErrorModes : uint
+        {
+            SYSTEM_DEFAULT = 0x0,
+            SEM_FAILCRITICALERRORS = 0x0001,
+            SEM_NOALIGNMENTFAULTEXCEPT = 0x0004,
+            SEM_NOGPFAULTERRORBOX = 0x0002,
+            SEM_NOOPENFILEERRORBOX = 0x8000
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)] static extern ErrorModes SetErrorMode(ErrorModes uMode);
+        
+        static void Main(string[] args)
+		{
+            SetErrorMode(ErrorModes.SEM_FAILCRITICALERRORS | ErrorModes.SEM_NOGPFAULTERRORBOX | ErrorModes.SEM_NOOPENFILEERRORBOX);
+
+            fileMagic = new Magic(String.Format("{0};{1}", GetPath("ncmagic.list"), GetPath("magic.mgc")), Magic.MAGIC_NONE | Magic.MAGIC_NO_CHECK_CDF);
+            mimeMagic = new Magic(String.Format("{0};{1}", GetPath("ncmagic.list"), GetPath("magic.mgc")), Magic.MAGIC_MIME_TYPE | Magic.MAGIC_NO_CHECK_CDF);
+
+			input = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding);
+            output = new StreamWriter(Console.OpenStandardOutput(), Console.OutputEncoding);
+            error = new StreamWriter(Console.OpenStandardError(), Console.OutputEncoding);
+
+			serializer = new JavaScriptSerializer();
+
+			if (args.Length == 1)
+			{
+				switch (args[0])
+				{
+					case "--stay_open":
+						EndlessOperation();
+						break;
+
+					case "--help":
+						PrintHelp();
+						break;
+
+					default:
+						output.Write(GetInfoJSON(args[0]));
+						if (!output.AutoFlush) output.Flush();
+						break;
+				}
+			}
+			else
+			{
+				PrintHelp();
+			}
+		}
+
+		private static void EndlessOperation()
+		{
+			while (!input.EndOfStream)
+			{
+				String line = input.ReadLine().Trim();
+				if (line == "exit")
+					break;
+
+				output.Write(GetInfoJSON(line));
+				output.WriteLine("{ready}");
+                if (!output.AutoFlush) output.Flush();
+			}
+		}
+
+		private static String GetInfoJSON(String filepath) 
+		{
+            String mimeType;
+			String fileType;
+
+			try
+			{
+				mimeType = mimeMagic.GetInfo(filepath);
+				fileType = fileMagic.GetInfo(filepath);
+			}
+			catch (Exception ex)
+			{
+                mimeType = "unknown";
+				fileType = "Error reading file";
+			}
+
+			StringBuilder stringBuilder = new StringBuilder();
+
+			stringBuilder.Append("{\n");
+			stringBuilder.AppendFormat("  \"MimeType\": {0},\n", serializer.Serialize(mimeType));
+			stringBuilder.AppendFormat("  \"FileType\": {0}\n", serializer.Serialize(fileType));
+			stringBuilder.Append("}\n");
+
+			return stringBuilder.ToString();
+		}
+
+		private static String GetPath(String fileName)
+		{
+			return Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, fileName);
+		}
+
+		private static void PrintHelp()
+		{
+			Console.Error.WriteLine("NetClean Magic Wrapper\n");
+			Console.Error.WriteLine("  Usage: NetClean.MagicWrapper.exe [file]");
+			Console.Error.WriteLine("           Get mimetype/filetype of single file\n");
+			Console.Error.WriteLine("         NetClean.MagicWrapper.exe --stay_open");
+			Console.Error.WriteLine("           Reads filename from stdin (one per line),");
+			Console.Error.WriteLine("           and prints back results to stdout");
+			Console.Error.WriteLine("           Each result is followed by a line containing \"{ready}\"\n");
+			Console.Error.WriteLine("         NetClean.MagicWrapper.exe --help");
+			Console.Error.WriteLine("           Displays this help");
+		}
+	}
+}
